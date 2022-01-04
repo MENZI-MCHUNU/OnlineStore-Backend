5c8e8707fb02431461bdd5cacc67176314f54755
MENZI-MCHUNU <MENZI-MCHUNU@users.noreply.github.com>
2022-01-04 18:19:40 +0200

    Updated Product Schema

--- mongo/models/user.js
+++ mongo/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
   name: {
     type: String,
     required: true,
@@ -21,4 +21,4 @@ const userSchema = new Schema({
   },
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema);
