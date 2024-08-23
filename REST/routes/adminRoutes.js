453fc3e5465a63b9d39ce199639338c7454467df
MENZI-MCHUNU <menzi639@gmail.com>
2024-08-23 16:04:49 +0200

    simple HTML and CSS to be changed later and use Reactjs
    Adding a simple HTML and CSS , later we will change this and use Reactjs for frontend.

--- REST/routes/adminRoutes.js
+++ REST/routes/adminRoutes.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const adminController = require('../controllers/adminController');
+const path = require('path');
+//const adminController = require('../controllers/adminController');
 
-router.post("/add-product", adminController.addProduct)
+//router.get("/add-product"/*, adminController.addProduct*/)
+router.get('/add-product',(req,res,next) =>{
+    res.sendFile(path.join(__dirname,'../../','views','add-product.html'))
+        
+});
+
+router.post('/add-product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const connectDB = require("./config/db");
-const cors = require("cors");
+//const connectDB = require("./config/db");
+//const cors = require("cors");
 const app = express();
-const router = require('./REST/routes/adminRoutes');
-const dotenv = require("dotenv");
-dotenv.config();
+const adminRouter = require('./REST/routes/adminRoutes');
+const shopRouter = require('./REST/routes/shopRoutes');
+//const dotenv = require("dotenv");
+//dotenv.config();
+const path = require('path');
 
 app.use(express.json());
 app.use(
@@ -13,15 +15,22 @@ app.use(
   })
 );
 
+app.use(express.static(path.join(__dirname,'public')))
 //Connect to the database
-connectDB();
+//connectDB();
 
 // enable CORS
-app.use(cors());
+//app.use(cors());
 
 //REST api routes
-app.use('/',router);
+app.use('/admin',adminRouter);
+app.use(shopRouter);
 
-app.listen(process.env.PORT, () => {
-  console.log("Listening on port : " + process.env.PORT);
+//error page for unknown route
+app.use((req,res,next) =>{
+  res.status(404).sendFile(path.join(__dirname,'views', '404.html'));
+})
+
+app.listen(3000/*process.env.PORT*/, () => {
+  console.log("Listening on port : ", 3000 /*+ process.env.PORT*/);
 });
